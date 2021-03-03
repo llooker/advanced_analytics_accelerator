9d35e035f447c2e74ef4748d1fa4c67da1858567
Shingi <samudzi@gmail.com>
2021-03-03 15:33:55 -0800

    Update BQ_auth.R

--- lookr/R/BQ_auth.R
+++ lookr/R/BQ_auth.R
@@ -1,6 +1,8 @@
 # Provide authentication
-## input auth.json file location
-provideBQAuthentication <- function(json_path="~/auth.json",projectid="project-id",datasetid="dataset-id",conn="bq_conn") 
+## Set GC_BQ_PROJECTID and GC_BQ_DATASETID and environment variables in your .Renviron
+
+provideBQAuthentication <- function(json_path="../service/auth.json",projectid=Sys.getenv("GC_BQ_PROJECTID"),
+                                    datasetid=Sys.getenv("GC_BQ_DATASETID"),conn="bq_conn") 
 {
   bq_auth(path = json_path)
   bq_auth(use_oob = TRUE)
